000b8c6bc21c0d9f1478ea5569803d4e9fd43645
AlixMental <ferchodaniel25@gmail.com>
2026-07-01 20:18:16 -0500

    Test1

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+ 
+app = Flask(_name_)
+ 
+@app.route('/')
+def index():
+  titulo = "FErnando Daniel Murillo Viv"
+  return render_template('index.html', titulo=titulo)
+ 
+if _name_ == '_main_':
+  app.run(host='0.0.0.0', debug=True, port=5000)
